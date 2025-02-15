b6e49df18efbc60550167a3b5a2c3e82e6b5b452
Trushna  Ramdas kaware <128932990+tk-glorygithub@users.noreply.github.com>
2025-02-16 01:13:59 +0530

    Time: 4 ms (99.71%) | Memory: 45.5 MB (59.55%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean isPalandrome(String s, int i, int j) {
         while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
 
-            } 
-            i++;
-            j--;
-                
+            } else
+                return false;
 
         }
         return true;
@@ -18,27 +17,16 @@ class Solution {
         int i = 0;
         int j = s.length()-1;
         while(i<j){
-            if(s.charAt(i)==s.charAt(j)){
-           i++;
-           j--;
+             if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
             
-        }
-        else{
+            }
+            else{
 
-            return (isPalandrome(s,i+1,j) || isPalandrome(s,i,j-1));
+               return (isPalandrome(s,i+1,j) || isPalandrome(s,i,j-1));
+           }
         }
-        }
-        
         return true;
-        }
-        
-    // wrong kyu aa raha mera to submit hogya - pata nhi yaar   kro ab run -nope   -Q husaar submit ho gaya tha n  are wo sirf run kiya tha ha ha
-
-// hogya submit mera 
-//
-
-    // wo test case konse me fail ho raha tha ?  write kro yaha pe -"atbbga"
-    // thumae bhi test case dikh rahe hongi n  - Q thumari submit ho ga
-
+    }  
 }
-// ye kya all cases // chup ab dry run krne do case k liye -ok *_*_*
