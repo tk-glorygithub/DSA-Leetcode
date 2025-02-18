f2ccf3f1b64e2a391cb34430809a60762921bbdb
Trushna  Ramdas kaware <128932990+tk-glorygithub@users.noreply.github.com>
2025-02-18 18:41:11 +0530

    Time: 180 ms (10.31%) | Memory: 83.9 MB (5.20%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,36 +1,26 @@
 class Solution {
-
     public int countPrimes(int n) {
-       if(n<=2){
+         ArrayList<Integer> Num = new ArrayList<>();
+        int arr [] = new int[n];
+    if(n==0 || n==1)return 0;
+         arr[0] = 1;
+         arr[1] =1;
 
-        return 0;
-    }
-   /* int count =0;
-      for(int  i = 2;i<=n;i++){
-        for(int j =2 ;j*j<=n;j++){
-            if(i%j ==0){
-                break;
-              
+        for(int i =2 ;i<n;i++ ){
+       if(arr[i] == 0){
+       for(int j = i ;j<n ;j+=i){
+         arr[j] =1;
+       }
+       arr[i] =0;
+
+       }
+
+        }
+        for(int i = 0;i<n;i++){
+            if(arr[i] ==0){
+                Num.add(i);
             }
-             count++; 
         }
-      }
-      return count; 
-  */
-
-  boolean[] composites = new boolean[n];
-  int limit = (int )Math.sqrt(n);
-for(int i =2;i <= limit ;i++){
-    if(composites[i] == false){
-        for(int j = i*i ;j <n; j+=i){
-            composites[j] = true;
-        }
-    }
-}
-  int count =0;
-  for(int i=2 ;i <n;i++){
-    if(composites[i] == false) count++;
-  }  
-  return count; 
+        return Num.size();
     }
 }
