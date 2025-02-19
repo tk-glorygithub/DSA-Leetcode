ebdb9b77968d30fb58171d05eeec3e1062cf4919
Trushna  Ramdas kaware <128932990+tk-glorygithub@users.noreply.github.com>
2025-02-19 14:18:01 +0530

    Time: 10 ms (31.14%) | Memory: 41.6 MB (51.48%) - LeetSync

---
+++ 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isFascinating(int n) {
+
+
+ String str = n+""+(n*2)+""+(n*3);  
+ boolean flag = true;
+    for( char ch ='1';ch <='9' ;ch++){  
+    int count =0;
+    for(int i =0;i<str.length();i++){
+        char c =str.charAt(i);
+        if(c ==ch)  {
+            count++;
+        }                
+
+    } 
+    if(count !=1){
+        flag =false;
+        break;
+    }
+    } 
+     return flag;
+    }
+
+   } 
