ab133ea5c5b743b817ef75738aaac2352571afbd
Trushna  Ramdas kaware <128932990+tk-glorygithub@users.noreply.github.com>
2025-02-21 21:36:26 +0530

    Time: 8 ms (60.00%) | Memory: 41.2 MB (73.50%) - LeetSync

--- 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.java
+++ 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.java
@@ -7,7 +7,7 @@ class Solution {
     for( char ch ='1';ch <='9' ;ch++){  
     int count =0;
     for(int i =0;i<str.length();i++){
-        char c =str.charAt(i);
+        char c =str.charAt(i); 
         if(c ==ch)  {
             count++;
         }                
