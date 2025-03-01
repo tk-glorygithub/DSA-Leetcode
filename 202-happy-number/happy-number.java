81ad2c00d1cf88435f9bb5a3bee2d4ed5156a5ac
Trushna  Ramdas kaware <128932990+tk-glorygithub@users.noreply.github.com>
2025-03-01 22:21:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (74.17%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,24 @@
+class Solution {
+
+
+
+    public boolean isHappy(int n) {
+      if(n ==1 ||n ==7){
+        return true;
+      }
+      else if(n<10){
+        return false;
+      }
+      else{
+        int sum =0;
+        while(n>0){
+            int rem = n%10;
+            sum += rem *rem;
+            n/=10;
+        }
+        return isHappy(sum);
+      }
+      
+}
+
+}
