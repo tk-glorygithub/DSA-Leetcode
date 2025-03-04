7a677fc7921b5c503f39a99583e6e9fa5bd08585
Trushna  Ramdas kaware <128932990+tk-glorygithub@users.noreply.github.com>
2025-03-05 00:12:40 +0530

    Time: 1 ms (27.27%) | Memory: 41.4 MB (8.69%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+           for (double i = 0; i < 31; i++) {
+            double ans = (double) Math.pow(4, i);
+            if (ans == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+    }
