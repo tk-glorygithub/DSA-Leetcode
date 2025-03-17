fa2cf2e8296782683a1d2158b801196b3d6b7d22
Trushna  Ramdas kaware <128932990+tk-glorygithub@users.noreply.github.com>
2025-03-17 14:19:15 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (65.68%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        
+       // char c = letters[0];
+        for(int i =0;i<letters.length ;i++){
+            if(letters[i] > target){
+               // c = letters[i];
+                return letters[i];
+            }
+        }
+        return letters[0];
+    }
+}
