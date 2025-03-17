df5371fa409803bd76f074802999d4dfa1136133
Trushna  Ramdas kaware <128932990+tk-glorygithub@users.noreply.github.com>
2025-03-17 14:19:55 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (50.06%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -1,13 +1,15 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
         
-       // char c = letters[0];
+        char c = letters[0];
         for(int i =0;i<letters.length ;i++){
             if(letters[i] > target){
-               // c = letters[i];
-                return letters[i];
+                c = letters[i];
+                //return letters[i];
+                return c;
             }
         }
-        return letters[0];
+        //return letters[0];
+        return c;
     }
 }
