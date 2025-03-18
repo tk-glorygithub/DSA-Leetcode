e25100c01980b572cac64cdae87c82a3bf6bef9f
Trushna  Ramdas kaware <128932990+tk-glorygithub@users.noreply.github.com>
2025-03-18 10:46:45 +0530

    Time: 2 ms (67.46%) | Memory: 41.8 MB (77.93%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,9 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) result ^= s.charAt(i);
+        for (int i = 0; i < t.length(); i++) result ^= t.charAt(i);
+        return (char) result;
+
+    }
+}
