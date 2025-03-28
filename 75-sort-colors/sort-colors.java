64f61cce755db26934054122c968696dc1e18996
Trushna  Ramdas kaware <128932990+tk-glorygithub@users.noreply.github.com>
2025-03-28 23:12:58 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (45.61%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,7 +1,25 @@
 class Solution {
     public void sortColors(int[] nums) {
         
-         Arrays.sort(nums);
-       //  return nums;
+       //  Arrays.sort(nums);
+       
+       int l =0;
+       int r = nums.length -1;
+
+       for(int i =0;i <=r;){
+        if(nums[i] ==0)
+        swap(nums,i++ ,l++);
+
+        else if(nums[i] ==1)
+        ++i;
+        else
+        swap(nums ,i,r--);
+       }
+       }
+      private void swap(int []nums ,int i ,int j){
+         int  temp  = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+      
     }
 }
