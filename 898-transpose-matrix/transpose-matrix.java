e669df251c4c050f8bf77c3f98f9b8517961a50d
Trushna  Ramdas kaware <128932990+tk-glorygithub@users.noreply.github.com>
2025-05-31 21:02:16 +0530

    Time: 1 ms (23.87%) | Memory: 45 MB (36.14%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.java
+++ 898-transpose-matrix/transpose-matrix.java
@@ -3,12 +3,10 @@ class Solution {
 
         int [][] result =  new int[matrix[0].length][matrix.length];
 
-       //   int[][] res = new int[matrix[0].length][matrix.length];
+     
         for(int i =0 ;i < matrix.length;i++){
             for(int j = 0 ;j< matrix[0].length ;j++){
-                // int temp = matrix[i][j];
-                // matrix[i][j] = matrix[j][i];
-                // matrix[j][i] = temp ;
+                
                 result[j][i] = matrix[i][j];
  
             }
