d74d2c984f8c279a70a255f70d7d5671aa189858
Trushna  Ramdas kaware <128932990+tk-glorygithub@users.noreply.github.com>
2025-07-04 00:26:49 +0530

    Time: 1 ms (99.88%) | Memory: 61.9 MB (41.31%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -2,12 +2,25 @@ class Solution {
     public int maxProfit(int[] prices) {
         
        int  mini = prices[0];
-       int maxProfit =0;
+       int maxProfit = 0;
+//   5-1=4
+//   3-1=2
+//   6-1=5
+//   4
+        
+    
        for(int i =0;i<prices.length;i++){
         int  cost =prices[i] -  mini;
         maxProfit = Math.max(maxProfit ,cost);
         mini = Math.min(mini ,prices[i]);
        }
         return  maxProfit;
+ 
+ 
+ 
+    
     }
 }
+
+
+  
