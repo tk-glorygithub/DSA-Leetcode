227ddf919c2f0b47cfef3816c67ba9b0e768c5c7
Trushna  Ramdas kaware <128932990+tk-glorygithub@users.noreply.github.com>
2025-09-06 01:11:52 +0530

    Time: 498 ms (38.49%) | Memory: 0B (100.00%) - LeetSync

---
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT name 
+FROM Customer
+WHERE  referee_id !=2 or
+      referee_id is NULL;
+
+
+#Customer.name >= 2 reference_id 
+#the customer name  = jane  , alex  , bill ,zack thhebbbs  name marks zack
