292d3136255e87db367e329e5fd3114b6c8af985
Trushna  Ramdas kaware <128932990+tk-glorygithub@users.noreply.github.com>
2025-09-14 18:39:07 +0530

    Time: 16 ms (0.00%) | Memory: 45.6 MB (100.00%) - LeetSync

---
+++ 3997-maximize-sum-of-at-most-k-distinct-elements/maximize-sum-of-at-most-k-distinct-elements.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] maxKDistinct(int[] nums, int k) {
+        
+        TreeSet<Integer> p = new TreeSet<Integer>();
+       for(int i =0;i<nums.length;i++)
+       {
+        p.add(nums[i]);
+
+       }
+	  System.out.println(p);
+	  ArrayList<Integer> L = new ArrayList<>();
+	  Iterator<Integer> desc = p.descendingIterator();
+	  int cnt =0;
+	  while(desc.hasNext() && cnt <k)
+		{
+		  L.add(desc.next());
+		 cnt++; 
+		 
+		  
+		}
+		 int[] result = new int[L.size()];
+        for (int i = 0; i < L.size(); i++) {
+            result[i] = L.get(i);
+        }
+
+        return result;
+    }
+}
